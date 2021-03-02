80a54f17af30b5379a1c7f89675ed13c3234b684
Yak! <yak1ex@users.noreply.github.com>
2021-03-02 12:24:59 +0900

    Reflect linter warnigng again.

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -22,11 +22,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -34,22 +31,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.navigate
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 data class Puppy(
     val name: String,
     val summary: String,
-    val description: String)
+    val description: String
+)
 
 val puppies = mapOf(
     "C++" to Puppy(
@@ -91,7 +88,7 @@ fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
         Column(
             modifier = Modifier.padding(24.dp)
-        ){
+        ) {
             Header()
             NavHost(navController, startDestination = "itemsView") {
                 composable("itemsView"){ ItemsView(navController) }
